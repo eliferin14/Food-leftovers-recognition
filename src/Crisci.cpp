d926b1d1f35b2e95f657f66e3a1593b8c612d530
cexxo <101983134+cexxo@users.noreply.github.com>
2023-06-09 15:52:28 +0200

    Sliced and feautured

--- src/Crisci.cpp
+++ src/Crisci.cpp
@@ -1,6 +1,7 @@
 #include "Crisci.h"
 #include<cmath>
 #include <opencv2/core/types.hpp>
+#include"Detector.hpp"
 using namespace std;
 using namespace cv;
 
@@ -66,4 +67,53 @@ void imageSlicer(vector<vector<Point2f>> coordinatePoints,Mat originalImage,vect
 		slicedImages.push_back(originalImage(Rect(temp_x1,temp_y1,temp_x2,temp_y2)));
 	}
 
+}
+
+void slicerViewer(vector<Mat>srcs, vector < vector<KeyPoint>>& keys, vector<Mat>& descriptors) {
+	for (int i = 0; i < srcs.size(); i++) {
+		vector<KeyPoint> temp;
+		Mat temp2;
+		keys.push_back(temp);
+		descriptors.push_back(temp2);
+		featureDetector(srcs[i], keys[i], descriptors[i]);
+	}
+}
+
+void slicedFeatureViewer(vector<Mat> slicedImages,vector<vector<KeyPoint>>slicedKeypoints) {
+	Scalar colorTab[] =
+	{
+		Scalar(0, 0, 255),
+		Scalar(0,255,0),
+		Scalar(255,100,100),
+		Scalar(255,0,255),
+		Scalar(0,255,255)
+	};
+	vector<vector<int>>slicedLabels(slicedImages.size());
+	vector<vector<Point2f>>slicedCenters(slicedImages.size());
+	vector<vector<Point2f>>slicedPoints(slicedImages.size());
+	TermCriteria criteria(TermCriteria::EPS | TermCriteria::MAX_ITER, 10, 1);
+	for (int j = 0; j < slicedImages.size(); j++) {
+		for (int i = 0; i < slicedKeypoints[j].size(); i++) {
+			//cout << "here" << j<< " "<<i<< endl;
+			slicedPoints[j].push_back(slicedKeypoints[j][i].pt);
+		}
+		kmeans(slicedPoints[j], 1, slicedLabels[j], criteria, 10, KMEANS_RANDOM_CENTERS, slicedCenters[j]);
+	}
+	//cout << (slicedPoints.size()==slicedImages.size()) << endl;
+	for (int j = 0; j < slicedImages.size(); j++) {
+		for (int i = 0; i < slicedCenters[j].size(); i++)
+		{
+			Point2f c = slicedCenters[j][i];
+			circle(slicedImages[j], c, 40, colorTab[j], 1, LINE_AA);
+		}
+		for (int i = 0; i < slicedPoints[j].size(); i++) {
+			Point2f c = slicedPoints[j][i];
+			circle(slicedImages[j], c, 10, colorTab[j], 1, LINE_AA);
+		}
+	}
+	for (int i = 0; i < slicedImages.size(); i++) {
+		namedWindow(to_string(i), WINDOW_NORMAL);
+		imshow(to_string(i), slicedImages[i]);
+		waitKey(0);
+	}
 }
